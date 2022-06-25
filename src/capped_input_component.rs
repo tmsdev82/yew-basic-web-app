eea27b37fae21c6a308348e207670b1ada96f959
Tim Steeman <tmdev82@gmail.com>
2022-06-25 11:24:27 +0200

    implement add to list button and list display

--- src/capped_input_component.rs
+++ src/capped_input_component.rs
@@ -1,8 +1,11 @@
 use web_sys::{HtmlInputElement};
-use yew::{Component, html, Html, Properties, InputEvent, TargetCast, NodeRef};
+use yew::{Component, html, Html, 
+    Properties, InputEvent, TargetCast, NodeRef, MouseEvent};
 
 pub enum Msg {
-    SetInput(u32)
+    SetInput(u32),
+    AddValue(u32),
+    DoNothing
 }
 
 #[derive(Properties, PartialEq)]
@@ -12,7 +15,8 @@ pub struct CappedInputProps {
 }
 
 pub struct CappedInputComponent {
-    input_ref: NodeRef
+    input_ref: NodeRef,
+    value_list: Vec<u32>
 }
 
 impl Component for CappedInputComponent {
@@ -20,7 +24,7 @@ impl Component for CappedInputComponent {
     type Properties = CappedInputProps;
 
     fn create(_ctx: &yew::Context<Self>) -> Self {
-        Self  { input_ref:NodeRef::default() }
+        Self  { input_ref:NodeRef::default(), value_list: Vec::new() }
     }
 
     fn update(&mut self, ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
@@ -30,6 +34,15 @@ impl Component for CappedInputComponent {
                 let input_element = self.input_ref.clone().cast::<HtmlInputElement>().unwrap();
                 input_element.set_value(&val.to_string());
                 true
+            },
+            Msg::AddValue(val) => {
+                log::debug!("Adding value to list: {}", val);
+                self.value_list.push(val);
+                true
+            },
+            Msg::DoNothing => {
+                log::debug!("Do nothing");
+                true
             }
         }
     }
@@ -38,6 +51,22 @@ impl Component for CappedInputComponent {
 
         let min_val = ctx.props().min_value;
         let max_val = ctx.props().max_value;
+
+        let on_click = {            
+            let cur_input = self.input_ref.clone();
+            ctx.link().callback(move |_e: MouseEvent| {
+                let value_input_element = cur_input.cast::<HtmlInputElement>().unwrap(); 
+                let new_value = value_input_element.value();
+                value_input_element.set_value("");
+                match new_value.parse() {
+                    Ok(val) => Msg::AddValue(val),
+                    Err(_) => {
+                        log::debug!("Error occured parsing '{}'", new_value);
+                        Msg::DoNothing
+                    }
+                }
+            })
+        };
 
         let on_input = ctx.link().callback(move |e: InputEvent| {
             let input_el: HtmlInputElement = e.target_unchecked_into();
@@ -59,11 +88,21 @@ impl Component for CappedInputComponent {
 
             Msg::SetInput(val)
         });
-        
+
+        let display_values = self.value_list.iter().map(|name| html!{<li>{name}</li>});
         html! {
             <div>
-                <label>{format!("Input value. Min: {}, Max: {}", min_val, max_val)}</label>
-                <input ref={self.input_ref.clone()} type="number" placeholder="input a number" oninput={on_input} min={min_val.to_string()} max={max_val.to_string()}/>              
+                <div>
+                    <label>{format!("Input value. Min: {}, Max: {}", min_val, max_val)}</label>
+                    <input ref={self.input_ref.clone()} type="number" placeholder="input a number" oninput={on_input} min={min_val.to_string()} max={max_val.to_string()}/>              
+                    <button onclick={on_click}>{"Add to list"}</button>
+                </div>
+                <div>
+                <ul>
+                 {for display_values}
+              
+                </ul>
+            </div>
             </div>
         }
     }
